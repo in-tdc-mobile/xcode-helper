5f5094c1760fbecdc6895510dddd5114c89f7b6a
vineethvijayan <Vineeth@tdc-vvijayan-mac.local>
2015-10-13 13:49:56 +0530

    upgrade to swift 2

--- GenerateSqLite Swift2.playground/Contents.swift
+++ GenerateSqLite Swift2.playground/Contents.swift
@@ -1,4 +1,6 @@
 //: Playground - noun: a place where people can play
+//Created by Vineeth Vijayan
+//
 
 import UIKit
 
@@ -15,19 +17,19 @@ result.append("")
 
 result.append("static func CreateTable(){")
 result.append("")
-result.append("let table = DataManager.db[\"" + EntityName + "\"]")
+result.append("let table = Table(\"" + EntityName + "\")")
 result.append("")
-result.append("DataManager.db.drop(table: table, ifExists: true)")
+result.append("try! DataManager.con.run(table.drop(ifExists: true))")
 for i in 0...(ElementList.count-1){
     let a = "let " + ElementList[i] + " = Expression<String?>(\"" + ElementList[i] + "\")"
     result.append(a)
 }
 result.append("")
-result.append("DataManager.db.create(table: table, ifNotExists: true) { t in")
+result.append("try! DataManager.con.run(table.create { t in")
 for i in 0...(ElementList.count-1){
     result.append("t.column(" + ElementList[i] + ")")
 }
-result.append("}")
+result.append("})")
 result.append("}")
 result.append("")
 result.append("static func Insert(entity :JSON){")
@@ -35,7 +37,7 @@ result.append("if entity.count == 0{")
 result.append("return")
 result.append("}")
 
-var str1 = "var bindStmt = DataManager.db.prepare(\"INSERT INTO " + EntityName + " ("
+var str1 = "let bindStmt = DataManager.con.prepare(\"INSERT INTO " + EntityName + " ("
 var str2 = ""
 for i in 0...(ElementList.count-1){
     str1 = str1 + ElementList[i] + ","
@@ -43,7 +45,7 @@ for i in 0...(ElementList.count-1){
 }
 result.append(str1.substringToIndex(str1.endIndex.predecessor()) + ") values (" + str2.substringToIndex(str2.endIndex.predecessor()) + ")\")")
 result.append("")
-result.append("let statement = DataManager.db.transaction { _ in")
+result.append("try! DataManager.con.transaction(block: {_ in")
 result.append("")
 result.append("for index in 0...(entity.count-1) {")
 
@@ -51,23 +53,19 @@ result.append("var bindings:[Binding?] = []")
 for i in 0...(ElementList.count-1){
     result.append("bindings.append(entity[index][\"" + ElementList[i] + "\"].string)")
 }
-result.append("bindStmt.run(bindings)")
+result.append("try! bindStmt.run(bindings)")
 result.append("}")
-result.append("return .Commit")
-result.append("}")
-result.append("if statement.failed {")
-result.append("// query failed")
-result.append("}")
+result.append("})")
 result.append("}")
 
 
 result.append("")
 
 result.append("static func GetAll() -> [" + EntityName + "]{")
-result.append("if !DataManager.db.tableExists(\"" + EntityName + "\"){")
+result.append("if !DataManager.con.tableExists(\"" + EntityName + "\"){")
 result.append("CreateTable()")
 result.append("}")
-result.append("let table = DataManager.db[\"" + EntityName + "\"]")
+result.append("let table = Table(\"" + EntityName + "\")")
 for i in 0...(ElementList.count-1){
     let a = "let " + ElementList[i] + " = Expression<String?>(\"" + ElementList[i] + "\")"
     result.append(a)
@@ -75,7 +73,7 @@ for i in 0...(ElementList.count-1){
 result.append("")
 result.append("var list:[" + EntityName + "] = []")
 
-result.append("for row in table {")
+result.append("for row in DataManager.con.prepare(table) {")
 result.append("let obj: " + EntityName + " = " + EntityName + "()")
 for i in 0...(ElementList.count-1){
     var a = "obj." + ElementList[i] + " = (row[" + ElementList[i] + "]) == nil ? \"\" "
@@ -90,7 +88,8 @@ result.append("}")
 
 
 for index in 0...(result.count-1){
-    println(result[index])
+    print(result[index]) //result[index])
 }
+
 
 
